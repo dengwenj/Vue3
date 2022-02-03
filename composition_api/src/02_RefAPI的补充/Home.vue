c6906d395630b9d03da158464231b12105e14449
dengwenj <927561237@qq.com>
2022-02-04 00:16:37 +0800

    toRef和toRefs的使用

---
+++ composition_api/src/02_RefAPI的补充/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h2>{{ age }}</h2>
+    <button @click="handleUpdate">更新</button>
+  </div>
+</template>
+
+<script>
+import { reactive, toRef, toRefs } from '@vue/reactivity'
+export default {
+  setup() {
+    const person = reactive({ name: 'dwj', age: 22 })
+    // 对 reactive 里面的所有属性都进行 ref
+    // const { name, age } = toRefs(person)
+
+    // 对其中的一个进行 ref
+    const age = toRef(person, 'age')
+
+    const handleUpdate = () => {
+      person.age++ // 这个也可以 他们是相互连接的
+      // age.value++
+    }
+
+    return {
+      name,
+      age,
+      handleUpdate
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- composition_api/src/App.vue
+++ composition_api/src/App.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script>
-import Home from './01_compositionAPI基本使用/Home.vue'
+// import Home from './01_compositionAPI基本使用/Home.vue'
+import Home from './02_RefAPI的补充/Home.vue'
 
 export default {
   components: {
