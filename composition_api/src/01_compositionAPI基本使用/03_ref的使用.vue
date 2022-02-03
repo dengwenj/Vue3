1d74c4a18814eeefedcd3ef966c75577127e03a2
dengwenj <927561237@qq.com>
2022-02-03 23:06:02 +0800

    readonly 的使用

---
+++ composition_api/src/01_compositionAPI基本使用/03_ref的使用.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{message}}
+    <button @click="handleClick">点击</button>
+    <!-- 不用 .value 帮我们解包了 -->
+    <h2>{{counter}}</h2>
+    <Demo :count="counter" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Demo from './Demo.vue'
+
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    Demo
+  },
+  setup() {
+    // ref 返回的一个可变的响应式对象
+    const counter = ref(100)
+
+    const handleClick = () => {
+      console.log(counter)
+      counter.value++
+    }
+
+    return {
+      handleClick,
+      counter
+    }
+  }
+}
+</script>
---
+++ composition_api/src/01_compositionAPI基本使用/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{count}}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      required: true
+    }
+  },
+  setup() {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
