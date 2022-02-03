05aee204011a9ddf047ee09becb47c3bcd375105
dengwenj <927561237@qq.com>
2022-02-04 00:38:56 +0800

    shallowRef和triggerRef

--- composition_api/src/02_RefAPI的补充/Home.vue
+++ composition_api/src/02_RefAPI的补充/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
+    <h2>{{ p }}</h2>
     <h2>{{ name }}</h2>
     <h2>{{ age }}</h2>
     <button @click="handleUpdate">更新</button>
+    <button @click="handleClick">点击</button>
   </div>
 </template>
 
 <script>
-import { reactive, toRef, toRefs } from '@vue/reactivity'
+import { reactive, shallowRef, toRef, toRefs, triggerRef, unref } from '@vue/reactivity'
 export default {
   setup() {
+    // 如果直接解构是直接把他的值赋值过去了 没有响应式 要用 toRefs
     const person = reactive({ name: 'dwj', age: 22 })
     // 对 reactive 里面的所有属性都进行 ref
     // const { name, age } = toRefs(person)
-
+    // 参数必须是一个 reactive 响应式对象
     // 对其中的一个进行 ref
     const age = toRef(person, 'age')
 
@@ -22,10 +25,22 @@ export default {
       // age.value++
     }
 
+    console.log(unref(age)) // 22
+
+    const p = shallowRef({ name: 'z' })
+
+    const handleClick = () => {
+      p.value.name = 'd'
+      triggerRef(p)
+    }
+
+
     return {
       name,
       age,
-      handleUpdate
+      p,
+      handleUpdate,
+      handleClick
     }
   }
 }
