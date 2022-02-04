59aa308be278e6b50126fc43e5ddae317ba6246c
dengwenj <927561237@qq.com>
2022-02-04 21:38:15 +0800

    jsx的使用

---
+++ composition_api/src/06_jsx的使用/Home.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  render(h) {
+    return <div>当前计数：{this.counter}</div>
+  }
+}
+</script>
--- composition_api/src/App.vue
+++ composition_api/src/App.vue
@@ -9,7 +9,8 @@
 // import Home from './02_RefAPI的补充/Home.vue'
 // import Home from './03_computed和watch/Home.vue'
 // import Home from './04_provide和Inject/Home.vue'
-import Home from './05_setup的顶层编写方式/Home.vue'
+// import Home from './05_setup的顶层编写方式/Home.vue'
+import Home from "./06_jsx的使用/Home.vue"
 
 export default {
   components: {
