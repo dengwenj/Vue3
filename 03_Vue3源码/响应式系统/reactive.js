3dc46b9c6759b0a0f484fe0f6019a9da331b45ef
dengwenj <927561237@qq.com>
2022-02-05 18:30:59 +0800

    vue2响应式

--- 03_Vue3源码/响应式系统/reactive.js
+++ 03_Vue3源码/响应式系统/reactive.js
@@ -19,16 +19,56 @@ class Dep {
 let activeEffect = null
 function watchEffect(effect) {
   activeEffect = effect
-  d.depend()
   effect()
   activeEffect = null
 }
 
-const info = {
-  counter: 100
+// Map({key: value}): key 是一个字符串
+// weakMap({key(对象): value}): key 是一个对象，弱引用
+const targetMap = new WeakMap()
+function getDep(target, key) {
+  // 1 根据对象（target）取出对应的 Map 对象
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  // 2 取出具体的 dep 对象
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Dep()
+    depsMap.set(key, dep)
+  }
+
+  return dep
 }
 
-const d = new Dep()
+function reactive(raw) {
+  Object.keys(raw).forEach((key) => {
+    const dep = getDep(raw, key)
+    let value = raw[key]
+
+    Object.defineProperty(raw, key, {
+      get() {
+        dep.depend()
+        return value
+      },
+      set(newValue) {
+        value = newValue
+        dep.notify()
+      }
+    })
+  })
+
+  return raw
+}
+
+const info = reactive({
+  counter: 100,
+  name: 'dwj'
+})
+console.log(info);
 
 watchEffect(function () {
   console.log(info.counter * 2);
@@ -39,4 +79,3 @@ watchEffect(function () {
 })
 
 info.counter++
-d.notify()
