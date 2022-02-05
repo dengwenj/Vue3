de5cdbfce11c3fe590b85e0fe20bc220728fbd1a
dengwenj <927561237@qq.com>
2022-02-05 16:01:02 +0800

    patch 函数props处理

--- 03_Vue3源码/渲染器的实现/render.js
+++ 03_Vue3源码/渲染器的实现/render.js
@@ -20,7 +20,7 @@ const mount = (vnode, container) => {
 
       // 对事件监听判断
       if (key.startsWith('on')) {
-        el.addEventListener(key.slice(2), value)
+        el.addEventListener(key.slice(2).toLowerCase(), value)
       } else {
         el.setAttribute(key, value)
       }
@@ -40,4 +40,43 @@ const mount = (vnode, container) => {
 
   // 4 将 el 挂载到 container 上
   container.appendChild(el)
+}
+
+const patch = (n1, n2) => {
+  if (n1.tag !== n2.tag) {
+    const parentEl = n1.el.parentElement
+    parentEl.removeChild(n1.el)
+    mount(n2, parentEl)
+  } else {
+    // 1 取出 element 对象， 并且在 n2 中进行保存
+    const el = n2.el = n1.el
+
+    // 2 处理 props
+    const oldProps = n1.props || {}
+    const newProps = n2.props || {}
+
+    // 获取所有的 newProps 添加到 el
+    for(const key in newProps) {
+      const oldValue = oldProps[key]
+      const newValue = newProps[key]
+
+      if (oldValue !== newValue) {
+        if (key.startsWith('on')) {
+          el.addEventListener(key.slice(2).toLowerCase(), newValue)
+        } else {
+          el.setAttribute(key, newValue)
+        }
+      }
+    }
+
+    // 删除旧的 props
+    for(const key in oldProps) {
+      const value = oldProps[key]
+      if (key.startsWith('on')) {
+        el.removeEventListener(key.slice(2).toLowerCase(), value)
+      } else {
+        el.setAttribute(key, value)
+      }
+    }
+  }
 }
