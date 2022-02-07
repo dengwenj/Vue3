982898473ee8c9201ac57aaa8a7ce3abd63a2a17
dengwenj <927561237@qq.com>
2022-02-07 19:30:32 +0800

    路由导航守卫

--- composition_api/src/router/index.js
+++ composition_api/src/router/index.js
@@ -51,6 +51,10 @@ const routes = [
     // 匹配不到任何路径会来到这里
     path: '/:pathMatch(.*)',
     component: () => import('../pages/NotFound.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('../pages/Login.vue')
   }
 ]
 
@@ -70,6 +74,29 @@ router.addRoute({
 router.addRoute('home', {
   path: '/home/monent',
   component: import('../pages/HomeMonent.vue')
+})
+
+console.log(router.hasRoute('home')); // true 里面写的是 name 名称 看存不存在
+
+/**
+ * to: Route 对象，即将跳转到的 Route 对象
+ * from: Route 对象，跳转之前的 Route 对象
+ */
+/**
+ * 返回值问题：
+ *   1 false：不进行导航
+ *   2 undefined或者不写返回值：进行默认导航
+ *   3 字符串：路径，跳转到对应的路径中
+ *   4 对象：{ path: '/login', query: {...} }
+ */
+router.beforeEach((to, from) => {
+  console.log('进行了跳转')
+
+  if (to.path.includes('/home')) {
+    return '/login'
+  }
+
+  // 后面不返回东西 进行的是默认导航，该到哪里就到哪里
 })
 
 export default router
