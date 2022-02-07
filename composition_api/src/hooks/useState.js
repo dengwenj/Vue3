a06391f9a605bd64242f617c8025e44ef65f1e58
dengwenj <927561237@qq.com>
2022-02-07 23:53:18 +0800

    注释下

--- composition_api/src/hooks/useState.js
+++ composition_api/src/hooks/useState.js
@@ -4,10 +4,11 @@ import { mapState, useStore } from "vuex"
 export default function (mappedState) {
   const store = useStore()
 
-  const objKeyFns = mapState(mappedState)
-  const storeState = {}
+  const objKeyFns = mapState(mappedState) // { name: function, age: function } 这里面的函数还是有用到 this 的拿的是 store 这个大对象
+  const storeState = {} // { name: ref, age: ref }
   Object.keys(objKeyFns).forEach(fnKey => {
-    storeState[fnKey] = computed(objKeyFns[fnKey].bind({ $store: store }))
+    const fn = objKeyFns[fnKey].bind({ $store: store })
+    storeState[fnKey] = computed(fn) // 返回的是 ref
   })
   return storeState
 }
