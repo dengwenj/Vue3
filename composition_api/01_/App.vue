6a0f8dfcb0ca8bb398d64a8b3efa55e8ad91c7b8
dengwenj <927561237@qq.com>
2022-04-22 00:15:47 +0800

    回顾vue3

---
+++ composition_api/01_/App.vue
@@ -0,0 +1,78 @@
+<!-- <template>
+  <div>
+    <Home message='我是 home 组件' />
+  </div>
+</template> -->
+
+<script setup>
+import { useRouter } from 'vue-router'
+// import Home from './01_compositionAPI基本使用/Home.vue'
+// import Home from './02_RefAPI的补充/Home.vue'
+// import Home from './03_computed和watch/Home.vue'
+// import Home from './04_provide和Inject/Home.vue'
+// import Home from './05_setup的顶层编写方式/Home.vue'
+// import Home from "./06_jsx的使用/Home.vue"
+// import Home from "./07_自定义指令/Home.vue"
+import Home from './components/Home.vue'
+
+// export default {
+//   components: {
+//     Home
+//   },
+//   setup() {
+
+//   }
+// }
+const name = 'dengwenj'
+const router = useRouter()
+
+const handleClick = (one) => {
+  console.log(one);
+  router.push({
+    // path: '/about',
+    // query: {
+    //   name: 'dwj',
+    //   age: 22
+    // }
+    name: 'about',
+    params: {
+      name: 'dwj',
+      age: 22
+    }
+  })
+}
+
+const goback = () => {
+  router.go(1)
+}
+</script>
+
+<template>
+  <div>
+    <!-- props: href 跳转的链接 -->
+    <!-- props: route对象 -->
+    <!-- props: navigate 导航函数 -->
+    <!-- props: isActive 是否当前处于活跃的状态 -->
+    <!-- props: isExactActive 是否当前处于精确的活跃状态 -->
+    <!-- <router-link to="/home" v-slot="props" custom>
+      <button @click="props.navigate">{{props.href}}</button>
+      <button @click="props.navigate">{{props.isActive}}</button>
+      <button @click="props.navigate">{{props.isExactActive}}</button>
+    </router-link>
+    <router-link to="/about">关于</router-link>
+    <router-link :to="`/user/${name}`">用户</router-link>
+    <button @click="handleClick(1)">关于</button>
+    <button @click="goback">前进后退</button>
+    <router-link to="/admin">admin</router-link>
+
+    <router-view v-slot="{ Component }">
+      <transition name="">
+        <keep-alive>
+          <component :is='Component'></component>
+        </keep-alive>
+      </transition>
+    </router-view> -->
+
+    <Home />
+  </div>
+</template>
---
+++ composition_api/01_/main.js
@@ -0,0 +1,37 @@
+import { createApp } from 'vue'
+
+import router from './router'
+import vuex from './store'
+import App from './App.vue'
+import directives from './08_directives'
+import pluginsObject from './09_plugins/plugins_object'
+import pluginsFunction from './09_plugins/plugins_function'
+
+const app = createApp(App)
+
+// 安装路由
+app.use(router) // 返回的是 app 对象
+// 安装 vuex
+app.use(vuex)
+
+app.use(pluginsObject)
+app.use(pluginsFunction)
+
+directives(app)
+
+// 全局的自定义指令
+// app.directive('focus', {
+//   mounted(el) {
+//     console.log(el)
+//     el.focus()
+//   },
+//   beforeMount(el) {
+//     console.log(el);
+//   },
+//   created(el) {
+//     console.log(el);
+//   }
+// })
+
+const c = app.mount('#app') // mount 返回的是组件对象
+console.log(c);
