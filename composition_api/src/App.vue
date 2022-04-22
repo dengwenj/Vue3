dcef1613232740aa3aafa5911c55c9b5f13cac6a
dengwenj <927561237@qq.com>
2022-04-22 15:53:26 +0800

    torefs使用

--- composition_api/src/App.vue
+++ composition_api/src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { ref, onMounted, onBeforeMount, reactive, toRef } from 'vue'
+import { ref, onMounted, onBeforeMount, reactive, toRef, toRefs } from 'vue'
 
 // 状态
 const count = ref(0)
 const person = reactive({
   name: 'dengwj',
   age: 22,
-  sex: '男'
+  sex: '男',
+  hobby: ['当社会人', '不睡觉']
 })
-const age = toRef(person, 'age')
-console.log(age)
+// const age = toRef(person, 'age')
+const { name, age, sex } = toRefs(person)
+
 // 钩子
 onBeforeMount(() => {
   console.log('onBeforeMount')
@@ -31,14 +33,18 @@ const handleName = () => {
 const handleAge = () => {
   age.value = 17
 }
+const handleSex = () => {
+  person.sex = '女'
+}
 </script>
 
 <template>
   <h1>{{count}}</h1>
-  <h2>{{person.age}}</h2>
-  <h2>{{person.name}}</h2>
-  <h2>{{person.sex}}</h2>
+  <h2>{{age}}</h2>
+  <h2>{{name}}</h2>
+  <h2>{{sex}}</h2>
   <button @click="handleClick">+1</button>
   <button @click="handleName">修改名字</button>
   <button @click="handleAge">修改名字</button>
+  <button @click="handleSex">修改性别</button>
 </template>
