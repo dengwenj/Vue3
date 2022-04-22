35ff4628412a73c027100bbf72704e028f537d53
dengwenj <927561237@qq.com>
2022-04-22 15:35:14 +0800

    toRef转换响应式对象中某个属性为单独响应式数据,并且是关联的

--- composition_api/src/App.vue
+++ composition_api/src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
-import { ref, onMounted, onBeforeMount } from 'vue'
+import { ref, onMounted, onBeforeMount, reactive, toRef } from 'vue'
 
+// 状态
 const count = ref(0)
-
+const person = reactive({
+  name: 'dengwj',
+  age: 22,
+  sex: '男'
+})
+const age = toRef(person, 'age')
+console.log(age)
+// 钩子
 onBeforeMount(() => {
   console.log('onBeforeMount')
 })
-
 onMounted(() => {
   console.log('onMounted1')
 })
@@ -14,14 +21,24 @@ onMounted(() => {
   console.log('onMounted2')
 })
 
+// 处理函数
 const handleClick = () => {
   count.value++
+}
+const handleName = () => {
+  person.name = 'ww'
+}
+const handleAge = () => {
+  age.value = 17
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{count}}</h1>
-    <button @click="handleClick">+1</button>
-  </div>
+  <h1>{{count}}</h1>
+  <h2>{{person.age}}</h2>
+  <h2>{{person.name}}</h2>
+  <h2>{{person.sex}}</h2>
+  <button @click="handleClick">+1</button>
+  <button @click="handleName">修改名字</button>
+  <button @click="handleAge">修改名字</button>
 </template>
